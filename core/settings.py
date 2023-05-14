e1c725d853cfb8cb6063854b9d830e64682ea67a
johnmarklumapac <102014089+johnmarklumapac@users.noreply.github.com>
2023-05-14 21:11:10 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0y5mv_+sipzpma_zi#+9w!__6gis67)3^m#@v0v!uwblwi5ck-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
